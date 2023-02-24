cf9d8079b2cad2e3441f3a26a58ff90b189836c5
JelaniM19 <126196356+JelaniM19@users.noreply.github.com>
2023-02-24 13:15:19 -0500

    Add files via upload

---
+++ CreateStudent.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+
+public class CreateStudent{
+
+
+	public static void main(String[] args) {
+
+		StudentDB student = new StudentDB(firstName, lastName, address, gender, age, dob, parent1, parent2, emerContact);
+		
+	}
+		
+
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
